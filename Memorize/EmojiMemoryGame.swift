cefae9a79e58807c2c7b4be3bc2eef102ca3ac0c
Camilo <camilossantos.2809@gmail.com>
2021-04-15 21:43:15 -0300

    add reactivity

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,13 +7,15 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+class EmojiMemoryGame: ObservableObject {
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String> = ["👻", "🎃", "🕷"]
         return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in emojis[pairIndex] }
     }
+    
+    
 //    MARK: - Access to the Model
     var cards:Array<MemoryGame<String>.Card>{
         model.cards
@@ -21,6 +23,7 @@ class EmojiMemoryGame {
     
 //    MARK: - Intent(s)
     func choose(card: MemoryGame<String>.Card){
+        objectWillChange.send()
         model.choose(card: card)
     }
 }
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Memorize
 //
 //  Created by Camilo Santos on 13/04/21.
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var viewModel: EmojiMemoryGame
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
         HStack{
@@ -17,7 +17,8 @@ struct ContentView: View {
                     viewModel.choose(card: card)
                 }
             }
-        }.padding()
+        }
+        .padding()
         .foregroundColor(.orange)
         .font(.largeTitle)
     }
@@ -42,6 +43,6 @@ struct CardView:View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        ContentView(viewModel: game)
+        EmojiMemoryGameView(viewModel: game)
     }
 }
